55c0a3c6a4c3142ac88cdc3403b34808317267f3
baekwinter <kksbiy0012@gmail.com>
2024-01-25 15:20:01 +0900

    240125_StoryOp_추가
    기존 스토리 오프닝 틀 추가 및 씬 추가

---
+++ Assets/Scripts/Story/MainStory.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MainStory : MonoBehaviour
+{
+    //OP : 누이를 위하여, 복수를 다짐한 남주인공 
+    public static void Openning()
+    {
+        Console.Clear();
+        Console.WriteLine("");
+        Console.ReadKey(true);
+        Console.WriteLine("");
+        Console.ReadKey(true);
+        Console.WriteLine("");
+        Console.ReadKey(true);
+        Console.WriteLine("");
+        Console.ReadKey(true);
+        Console.WriteLine("");
+        Console.ReadKey(true);
+        Console.WriteLine("");
+        Console.ReadKey(true);
+        Console.WriteLine("");
+        Console.ReadKey(true);
+
+        // 대화를 기다리며 화면을 지연시킵니다.
+        Console.WriteLine("\n");
+        Thread.Sleep(1500);
+    }
+
+
+}
