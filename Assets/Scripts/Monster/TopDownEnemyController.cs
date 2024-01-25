3a902b5f747a0d4cf4b37ff405fd72e94f678e99
baedonggeun <hispanie3@gmail.com>
2024-01-25 18:40:19 +0900

    240125_monster_근접 monster 기본 기능 구현
    1. TopDownEnemyController.cs 에서 몬스터와 플레이어의 거리, 방향(단위벡터) 계산
    2. TopDownContactEnemyController.cs 에서 몬스터 인식 범위 내로 tag가 Player인 sprite가 접근 시, Player를 보도록 monster sprite를 회전시키고, 따라가도록 구현
    3. TopDownCharacterController.cs 에서 CallMonveEvent함수 TopDownContactEnemyController.cs에서 작동하지 않는 버그 있음

---
+++ Assets/Scripts/Monster/TopDownEnemyController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownEnemyController : TopDownCharacterController
+{
+    GameManager gameManager;
+
+    protected Transform ClosetTarget { get; private set; }      //충돌하러 가는 몬스터 위치
+
+    //protected override void Awake()
+    //{
+    //    base.Awake();
+    //}
+    /*
+     * TopDownCharacterController 에서 Awake() 함수를 상속 받아 사용하지만 
+     * 해당 클래스에서 Awake() 함수를 사용하지 않아 주석 처리
+     * 해당 클래스에서 Awake() 함수에 CharacterStatsHandler.cs 를 만들어 해당 stats를 작성하고
+     * stat을 받아오는 식으로 작성하면 좋을듯
+     */
+
+    protected virtual void Start()      //게임매니저, 플레이어 호출
+    {
+        gameManager = GameManager.instance;
+        ClosetTarget = gameManager.Player;
+    }
+
+    protected virtual void FixedUpdate()
+    {
+
+    }
+
+    protected float DistanceToTarget()      //플레이어와 몬스터의 거리 계산
+    {
+        return Vector3.Distance(transform.position, ClosetTarget.position);
+    }
+
+    protected Vector2 DirectionToTarget()       //플레이어와 몬스터의 사이의 단위 벡터 계산(방향)
+    {
+        return (ClosetTarget.position - transform.position).normalized;
+    }
+}
